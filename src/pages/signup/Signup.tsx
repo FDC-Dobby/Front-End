9096ba73705fc6a6b50c6d0c78eb21f0fe463033
dya-only <dyacode@proton.me>
2023-10-25 14:07:28 +0900

    feat: signup value

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import styles from './signup.module.css';
 import Button from '../../components/button/component';
-import SInput from '../../components/sinput/component';
 import classNames from 'classnames';
-
 
 export default function Signup() {
   const [n, setN] = useState<number>(1);
   const [user, setUser] = useState({
-    name: String,
-    birth: String,
-    gender: String,
-    contact: String,
-    email: String,
-    userid: String,
-    password: String
+    name: '',
+    birth: '',
+    gender: '',
+    contact: '',
+    email: '',
+    userid: '',
+    password: '',
+    repassword: ''
+  })
+  const [agree, setAgree] = useState({
+    terms: false,
+    notificate: false
   })
 
   const nextLevel = () => {
@@ -39,43 +42,47 @@ export default function Signup() {
           <div className={styles.contain}>
             <div className={styles.el}>
               <div className={styles.tag}>이름</div>
-              <SInput placeholder={''} type={'text'} />
+              {/* <SInput placeholder={''} type={'text'} /> */}
+              <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, name: e.target.value })} />
             </div>
             <div className={styles.el}>
               <div className={styles.tag}>생년월일</div>
-              <SInput placeholder={''} type={'text'} />
+              {/* <SInput placeholder={''} type={'text'} /> */}
+              <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, birth: e.target.value })} />
             </div>
             <div className={styles.el}>
               <div className={styles.tag}>성별</div>
-              <SInput placeholder={''} type={'text'} />
+              {/* <SInput placeholder={''} type={'text'} /> */}
+              <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, gender: e.target.value })} />
             </div>
             <div className={styles.el}>
               <div className={styles.tag}>전화번호</div>
-              <SInput placeholder={''} type={'text'} />
+              {/* <SInput placeholder={''} type={'text'} /> */}
+              <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, contact: e.target.value })} />
             </div>
           </div>
           : (n == 2 ?
             <div className={styles.contain}>
               <div className={styles.el}>
                 <div className={styles.tag}>이메일</div>
-                <SInput placeholder={''} type={'text'} />
+                <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, email: e.target.value })} />
               </div>
               <div className={styles.el}>
                 <div className={styles.tag}>아이디</div>
-                <SInput placeholder={''} type={'text'} />
+                <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, userid: e.target.value })} />
               </div>
             </div>
             : (n == 3 ? <div className={styles.contain}>
               <div className={styles.el}>
                 <div className={styles.tag}>비밀번호</div>
-                <SInput placeholder={''} type={'password'} />
+                <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, password: e.target.value })} />
               </div>
               <div className={styles.el}>
                 <div className={styles.tag}>비밀번호 확인</div>
-                <SInput placeholder={''} type={'password'} />
+                <input type={'text'} className={styles.sinput} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, repassword: e.target.value })} />
               </div>
 
-              <div className={classNames([styles.check, styles.empty])}>
+              <div className={classNames([styles.check, styles.empty])} onClick={() => setAgree({ ...agree, terms: (agree.terms ? false : true) })}>
                 <label className={styles.checkbox}>
                   <input type="checkbox" />
                   <span className={styles.checkbox_icon}></span>
@@ -83,7 +90,7 @@ export default function Signup() {
                 </label>
                 <div>이즈프리의 이용약관에 동의합니다. (필수)</div>
               </div>
-              <div className={styles.check}>
+              <div className={styles.check} onClick={() => setAgree({ ...agree, notificate: (agree.notificate ? false : true) })}>
                 <label className={styles.checkbox}>
                   <input type="checkbox" />
                   <span className={styles.checkbox_icon}></span>
@@ -92,11 +99,11 @@ export default function Signup() {
                 <div>이즈프리의 알람에 동의합니다. (선택)</div>
               </div>
             </div>
-            : <div className={styles.contain}>회원가입이<br/>완료되었습니다.</div> )
+              : <div className={styles.contain}>회원가입이<br />완료되었습니다.</div>)
           )}
       </div>
 
-      <div className={styles.btn} onClick={nextLevel}><Button>{ n == 4 ? '완료' : '다음' }</Button></div>
+      <div className={styles.btn} onClick={nextLevel}><Button>{n == 4 ? '완료' : '다음'}</Button></div>
     </div>
   )
 }
